3e98fec791e2d8b4b602ebc1cbfbf1a7bd859f80
Konstantin Astapov <mind_bk@bk.ru>
2018-10-19 20:41:19 +0600

    dev::material header added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <div style="text-align: center">
+      <mat-toolbar color="primary">
+        <span>LocalCast Weather</span>
+      </mat-toolbar>
+      <div>Your city, your forecast, right now!</div>
+      <h2>Current Weather</h2>
+      <app-current-weather></app-current-weather>
+    </div>
+  `
 })
 export class AppComponent {}
